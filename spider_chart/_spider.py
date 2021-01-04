b7dc51ba34c8526bbb7d77698e16a823455fd06f
Norman <normanius@gmail.com>
2021-01-04 03:43:33 +0100

    Refactor/fixing filling and uncertainty bounds

--- spider_chart/_spider.py
+++ spider_chart/_spider.py
@@ -3,23 +3,30 @@ import pandas as pd
 import seaborn as sns
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from matplotlib.patches import Polygon
+import warnings
 
 
 def _ensure_axes(ax, enforce):
     if ax is None:
-        if enforce:
-            _, ax = plt.subplots(subplot_kw={"polar": True})
-        else:
+        # Get axes without creating new one.
+        fig = plt.gcf()
+        if fig.axes:
             ax = plt.gca()
-    if isinstance(ax, mpl.axes.Axes) and ax.name != "polar":
-        msg = ("Axes must use polar projection. Use one of the following "
-               "statements to ensure polar projection:\n"
-               "    plt.gca(polar=True)\n"
-               "    ax = plt.subplot(..., polar=True)\n"
-               "    fig, ax = plt.subplots(subplot_kw={'polar': True})\n"
-               )
-        raise ValueError(msg)
-    return ax
+    if isinstance(ax, mpl.axes.Axes) and ax.name == "polar":
+        return ax
+    else:
+        if enforce:
+            ax = plt.gca(polar=True)
+            return ax
+        else:
+            msg = ("Axes must use polar projection. Use one of the following "
+                   "statements to ensure polar projection:\n"
+                   "    plt.gca(polar=True)\n"
+                   "    ax = plt.subplot(..., polar=True)\n"
+                   "    fig, ax = plt.subplots(subplot_kw={'polar': True})\n"
+                   )
+            raise ValueError(msg)
 
 
 def _format_input_data(x, y, hue, style, data):
@@ -27,6 +34,7 @@ def _format_input_data(x, y, hue, style, data):
     if (y is None and data is None):
         raise ValueError("Arguments y and data cannot both be None.")
     if data is None:
+        # Array mode: only x (optionally) and y are provided.
         if y is None:
             msg = "In array mode (data=None), argument y must be set."
             raise ValueError(msg)
@@ -37,12 +45,23 @@ def _format_input_data(x, y, hue, style, data):
         data = pd.DataFrame(y).set_index(x)
         data.index.name = "x"
         data.columns.name = "category"
-        # For pandas > 1.1:
-        # data = data.melt(ignore_index=False, value_name="value").reset_index()
         data = data.reset_index().melt(value_name="value", id_vars="x")
         x = "x"
         y = "value"
-        hue = "category"
+        # The case where y.shape[1]=d>1 can be resolved in two ways:
+        #   1) hue="category": Each column represents a separate category,
+        #      represented as differently colored lines in the plot
+        #   2) hue=None: Each row represents d samples of the same category.
+        #      Before plotting, the data is aggregated along axis=1. A single
+        #      line is plotted by sns.lineplot ± uncertainty bounds.
+        # Problem: Choice 2) is easy by setting hue=None. However, choice 1)
+        # cannot be made by the user, because hue should be "category", which
+        # is an arbitrarily chosen, internal variable name.
+        # Resolution: introduce magic setting hue="columns", which applies
+        # only to this array mode. I think this is most consistent with
+        # seaborn.
+        if isinstance(hue, str) and hue=="columns":
+            hue = "category"
         fmt = "long"
     elif isinstance(data, pd.Series):
         data.name = "value" if data.name is None else data.name
@@ -116,9 +135,35 @@ def _adjust_polar_grid(ax, vals, labels,
 
 def _fill_and_close(ax, data, extent, lines_old,
                     fill, fillalpha, fillcolor, kwargs):
-    # This is the fragile part and modify/add the artists.
-    # - Close the lines
-    # - Create a polygon patch if fill=True
+    """
+    This is the fragile part and modify/add the artists.
+    - Close the lines
+    - Create a polygon patch if fill=True
+    """
+    def _draw_poly(xy, color, alpha, ax):
+        poly = Polygon(xy, closed=True, fc=color, alpha=alpha)
+        poly.set_fc(color)
+        ax.add_patch(poly)
+
+    def _draw_extent(xy, extent, data, color, alpha, ax):
+        if data is not None:
+            extent = data.get(extent, extent)
+        extent = np.asarray(extent)
+        xy = np.concatenate([xy, [xy[0]]], axis=0)
+        extent = np.append(extent, extent[0])
+        xy1 = xy.copy()
+        xy1[:,1] += extent
+        xy2 = xy.copy()
+        xy2[:,1] -= extent
+        xy = np.concatenate([xy1, xy2[::-1]], axis=0)
+        _draw_poly(xy=xy, color=color, alpha=alpha, ax=ax)
+
+    if not fill and extent is None:
+        return
+
+    draw_fill = fill
+    draw_extent = extent is not None
+    close_lines = True
 
     # If kwargs["label"] exists, the new line artist will carry that label.
     # Line2D.get_label() might be None, though it should always be a string.
@@ -128,38 +173,30 @@ def _fill_and_close(ax, data, extent, lines_old,
                  ((has_label and l.get_label()==label) or
                   (not has_label and l.get_label().startswith("_line")))]
 
-    from matplotlib.patches import Polygon
     patches = []
     for _,l in lines_new:
-        xy = l.get_xydata()
         # Filter nan-valued items. This step is necessary for seaborn>=0.10.
-        xy = xy[~np.isnan(xy).any(axis=1)]
+        xy = l.get_xydata()
+        mask = np.isnan(xy).any(axis=1)
+        xy = xy[~mask]
+        xyp = xy.copy()
 
-        if fill:
-            # Add patches.
-            alpha = fillalpha if fillalpha is not None else l.get_alpha()
-            color = fillcolor if fillcolor is not None else l.get_color()
-            xyp = xy.copy()
+        alpha = fillalpha if fillalpha is not None else l.get_alpha()
+        color = fillcolor if fillcolor is not None else l.get_color()
 
-            if extent is not None:
-                if data is not None:
-                    extent = data.get(extent, extent)
-                extent = np.asarray(extent)
-                xyp = np.concatenate([xyp,[xyp[0]]], axis=0)
-                extent = np.append(extent, extent[0])
-                xy1 = xyp.copy()
-                xy1[:,1] += extent
-                xy2 = xyp.copy()
-                xy2[:,1] -= extent
-                xyp = np.concatenate([xy1,xy2[::-1]], axis=0)
+        if draw_extent:
+            if mask.any():
+                msg = "Extent is skipped for line containing nan data."
+                warnings.warn(msg)
+            else:
+                _draw_extent(xy=xyp, extent=extent, data=data,
+                             color=color, alpha=alpha, ax=ax)
+        if draw_fill:
+            _draw_poly(xy=xyp, color=color, alpha=alpha, ax=ax)
 
-            poly = Polygon(xyp, closed=True, fc=color, alpha=alpha)
-            poly.set_fc(color)
-            ax.add_patch(poly)
-        # Close lines.
-        if True:
+        if close_lines:
             xdata, ydata = l.get_xdata(), l.get_ydata()
-            # pandas>=0.10 doesn't skip nan-points, leading to interrupted
+            # pandas>=0.10 doesn't skip nan-points, leading to interrupted
             # lines (with the segments that use a nan-point missing). I
             # consider this the right behavior. If the nan-filtered xy
             # should be used:
