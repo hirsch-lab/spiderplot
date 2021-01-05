7f4e08ec5e61ec136c1848e2361f6cbb31ed3a89
Norman <normanius@gmail.com>
2021-01-05 03:10:15 +0100

    Add some demo functions

--- spiderplot/__init__.py
+++ spiderplot/__init__.py
@@ -2,3 +2,7 @@ __version__ = "0.1.0"
 __author__ = "Norman Juchler"
 
 from ._spider import (spiderplot, spiderplot_facet)
+from ._demo import (demo_pair,
+                    demo_multi,
+                    demo_single,
+                    demo_aggregate)
